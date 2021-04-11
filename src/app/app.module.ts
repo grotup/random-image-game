480d3aebc1d565b275c54f115de8cf2ed15e25b8
Sylvain Nael <sylvain.nael@rca.fr>
2021-04-11 14:47:08 +0200

    feat: le menu dans un composant à part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatButtonModule} from '@angular/material/button'; 
-import {MatToolbarModule} from '@angular/material/toolbar'; 
-import {MatTabsModule} from '@angular/material/tabs'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 
-import {MatSidenavModule} from '@angular/material/sidenav'; 
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlateauComponent } from './plateau/plateau.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlateauComponent
+    PlateauComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
